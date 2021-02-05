b1b3d1c5a74bf58fc670650b2be569cfb7bb9342
Linda Hendricks <linda.timoh@gmail>
2021-02-05 10:05:36 -0500

    commit tree 47861caa241635b112050d82db2e2365993f5f56

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
+require 'pry'
+class CashRegister
 
+    attr_reader 
+    attr_accessor :e_discount, :total
+
+    def initialize(total)
+        @total = 0
+        @e_discount = e_discount
+    end
+    
+    def total
+        @total
+    end 
+
+    #binding.pry
+end
+
+cash1 = CashRegister.new(3)
+cash2 = CashRegister.new(4)
+
+puts cash2.total 
+puts cash1
+
+
+
+binding.pry 
